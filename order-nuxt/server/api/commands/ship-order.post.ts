0a38b9ff59fbaae07aa7f84a63c12d768a46839b
Mattias Holmqvist <mattias@serialized.io>
2023-01-24 16:57:06 +0100

    Update to client version 7.2.0

--- order-nuxt/server/api/commands/ship-order.post.ts
+++ order-nuxt/server/api/commands/ship-order.post.ts
@@ -4,7 +4,7 @@ import {Order} from "~/server/utils/domain/order/order";
 export default defineEventHandler(async (request) => {
   const {orderId} = await readBody(request)
   const client = request.context.serialized.aggregateClient(Order);
-  await client.update(orderId, (order: Order) => {
+  await client.update({aggregateId: orderId}, (order: Order) => {
     return order.ship(new Date().getTime())
   })
   console.log({message: 'Order shipped', orderId})
